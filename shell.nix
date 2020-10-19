eafc5471b8d68a720e62bd01aed001e472e2db21
James Brock <jamesbrock@gmail.com>
2020-10-20 00:34:00 +0900

    Trying to get system-monitor working

---
+++ shell.nix
@@ -0,0 +1,40 @@
+{ overlays ? []
+, config ? {}
+, pkgs ? import ./nix { inherit config overlays; }
+}:
+
+let
+  jupyter = import ./default.nix { inherit config overlays pkgs; };
+
+  iPython = jupyter.kernels.iPythonWith {
+    name = "python";
+    packages = p: with p; [ numpy ];
+  };
+
+  # https://github.com/yuvipanda/nbresuse
+  # Needed for jupyterlab-system-monitor
+  nbresuse = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "nbresuse";
+    version = "0.3.6";
+    src = pkgs.python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "13p9chk341iy78zwa2vmrnqkysi70wpxlk5kmrq99wcgn389v8av";
+    };
+    propagatedBuildInputs = with pkgs.python3Packages; [ notebook psutil mock ];
+  };
+
+  jupyterEnvironment =
+    jupyter.jupyterlabWith {
+      kernels = [ iPython ];
+      directory = jupyter.mkDirectoryWith {
+        extensions = [
+          "jupyterlab-topbar-extension@0.5.0"
+          "jupyterlab-system-monitor@0.6.0"
+        ];
+      };
+      extraPackages = p: [ nbresuse ];
+      extraInputsFrom = _: [ nbresuse ];
+      extraJupyterPath = _: "${nbresuse}/lib/python3.8/site-packages";
+    };
+in
+  jupyterEnvironment.env
